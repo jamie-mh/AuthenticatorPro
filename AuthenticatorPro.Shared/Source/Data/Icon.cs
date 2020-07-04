42e80d4fd34b354520b76377628bd2155d065bfc
Schokobecher <Schokobecher@gmail.com>
2020-07-04 22:33:43 +0700

    Added UnlockBase (unlockbase.com) Icon

--- AuthenticatorPro.Shared/Source/Data/Icon.cs
+++ AuthenticatorPro.Shared/Source/Data/Icon.cs
@@ -103,6 +103,7 @@ namespace AuthenticatorPro.Shared.Data
             {"twitter", Resource.Drawable.auth_twitter},
             {"uber", Resource.Drawable.auth_uber},
             {"ubisoft", Resource.Drawable.auth_ubisoft},
+            {"unlockbase", Resource.Drawable.auth_unlockbase},
             {"uphold", Resource.Drawable.auth_uphold},
             {"uplay", Resource.Drawable.auth_uplay},
             {"uptimerobot", Resource.Drawable.auth_uptimerobot},
