ad6a15dcdaf5397b59d4f356c503570b8792d871
Jamie Hodgson <jamie-hodgson@live.com>
2021-01-10 10:45:04 +0100

    remove using

--- AuthenticatorPro/Source/Data/Backup/HtmlBackup.cs
+++ AuthenticatorPro/Source/Data/Backup/HtmlBackup.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using Android.Content;
-using AuthenticatorPro.Shared.Data;
 using QRCoder;
 
 namespace AuthenticatorPro.Data.Backup
