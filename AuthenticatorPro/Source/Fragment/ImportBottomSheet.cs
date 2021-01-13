2c21f64663747b80c7fa09f911b2df0fd5747bdf
Jamie Hodgson <jamie-hodgson@live.com>
2021-01-13 16:43:21 +0100

    add hints to import

--- AuthenticatorPro/Source/Fragment/ImportBottomSheet.cs
+++ AuthenticatorPro/Source/Fragment/ImportBottomSheet.cs
@@ -28,15 +28,15 @@ namespace AuthenticatorPro.Fragment
             var menu = view.FindViewById<RecyclerView>(Resource.Id.listMenu);
             SetupMenu(menu, new List<SheetMenuItem>
             {
-                new(Resource.Drawable.ic_googleauthenticator, Resource.String.googleAuthenticator, ClickGoogleAuthenticator),
-                new(Resource.Drawable.ic_authenticatorplus, Resource.String.authenticatorPlus, ClickAuthenticatorPlus),
-                new(Resource.Drawable.ic_andotp, Resource.String.andOtp, ClickAndOtp),
-                new(Resource.Drawable.ic_freeotpplus, Resource.String.freeOtpPlus, ClickFreeOtpPlus),
-                new(Resource.Drawable.ic_aegis, Resource.String.aegis, ClickAegis),
-                new(Resource.Drawable.ic_winauth, Resource.String.winAuth, ClickWinAuth),
-                new(Resource.Drawable.ic_totpauthenticator, Resource.String.totpAuthenticator, ClickTotpAuthenticator),
-                new(Resource.Drawable.auth_steam, Resource.String.steam, ClickSteam),
-                new(Resource.Drawable.auth_blizzard, Resource.String.blizzardAuthenticator, ClickBlizzardAuthenticator)
+                new(Resource.Drawable.ic_googleauthenticator, Resource.String.googleAuthenticator, ClickGoogleAuthenticator, Resource.String.viewGuideImportHint),
+                new(Resource.Drawable.ic_authenticatorplus, Resource.String.authenticatorPlus, ClickAuthenticatorPlus, Resource.String.authenticatorPlusImportHint),
+                new(Resource.Drawable.ic_andotp, Resource.String.andOtp, ClickAndOtp, Resource.String.andOtpImportHint),
+                new(Resource.Drawable.ic_freeotpplus, Resource.String.freeOtpPlus, ClickFreeOtpPlus, Resource.String.freeOtpPlusImportHint),
+                new(Resource.Drawable.ic_aegis, Resource.String.aegis, ClickAegis, Resource.String.aegisImportHint),
+                new(Resource.Drawable.ic_winauth, Resource.String.winAuth, ClickWinAuth, Resource.String.winAuthImportHint),
+                new(Resource.Drawable.ic_totpauthenticator, Resource.String.totpAuthenticator, ClickTotpAuthenticator, Resource.String.totpAuthenticatorImportHint),
+                new(Resource.Drawable.auth_steam, Resource.String.steam, ClickSteam, Resource.String.viewGuideImportHint),
+                new(Resource.Drawable.auth_blizzard, Resource.String.blizzardAuthenticator, ClickBlizzardAuthenticator, Resource.String.viewGuideImportHint)
             });
 
             return view;
