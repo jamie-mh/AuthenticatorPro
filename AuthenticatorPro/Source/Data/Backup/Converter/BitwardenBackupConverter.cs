ec009b382845a2904d4d784c2d2ab0b09bbca7e0
Jamie Hodgson <jamie-hodgson@live.com>
2021-01-17 14:03:25 +0100

    convert bitwarden folders, steam and make sure type is login

--- AuthenticatorPro/Source/Data/Backup/Converter/BitwardenBackupConverter.cs
+++ AuthenticatorPro/Source/Data/Backup/Converter/BitwardenBackupConverter.cs
@@ -11,21 +11,58 @@ namespace AuthenticatorPro.Data.Backup.Converter
     internal class BitwardenBackupConverter : BackupConverter
     {
         public override BackupPasswordPolicy PasswordPolicy => BackupPasswordPolicy.Never;
-        private const AuthenticatorType Type = AuthenticatorType.Totp;
+        private const int LoginType = 1;
         
         public override Task<Backup> Convert(byte[] data, string password = null)
         {
             var json = Encoding.UTF8.GetString(data);
             var export = JsonConvert.DeserializeObject<Export>(json);
-            var authenticators = export.Items.Where(item => !String.IsNullOrEmpty(item.Login.Totp)).Select(item => item.Convert()).ToList();
 
-            return Task.FromResult(new Backup(authenticators));
+            var convertableItems = export.Items.Where(item => item.Type == LoginType && item.Login != null && !String.IsNullOrEmpty(item.Login.Totp)).ToList();
+            
+            var authenticators = convertableItems.Select(item => item.Convert()).ToList();
+            var categories = export.Folders.Select(f => f.Convert()).ToList();
+            var bindings = new List<AuthenticatorCategory>();
+
+            for(var i = 0; i < convertableItems.Count; ++i)
+            {
+                var folderId = convertableItems[i].FolderId;
+
+                if(folderId == null)
+                    continue;
+
+                var folderName = export.Folders.First(f => f.Id == folderId).Name;
+                var category = categories.First(c => c.Name == folderName);
+                var auth = authenticators[i];
+                
+                bindings.Add(new AuthenticatorCategory(auth.Secret, category.Id)); 
+            }
+
+            return Task.FromResult(new Backup(authenticators, categories, bindings));
         }
 
         private class Export
         {
+            [JsonProperty(PropertyName = "folders")]
+            public List<Folder> Folders { get; set; }
+            
             [JsonProperty(PropertyName = "items")]
             public List<Item> Items { get; set; }
+        }
+
+        private class Folder
+        {
+            [JsonProperty(PropertyName = "id")]
+            public string Id { get; set; }
+            
+            [JsonProperty(PropertyName = "name")]
+            public string Name { get; set; }
+          
+            
+            public Category Convert()
+            {
+                return new(Name);
+            }
         }
 
         private class Item
@@ -33,26 +70,43 @@ namespace AuthenticatorPro.Data.Backup.Converter
             [JsonProperty(PropertyName = "name")]
             public string Name { get; set; }
             
+            [JsonProperty(PropertyName = "folderId")]
+            public string FolderId { get; set; }
+            
+            [JsonProperty(PropertyName = "type")]
+            public int Type { get; set; }
+            
             [JsonProperty(PropertyName = "login")]
-            public Login Login { get; set; }
+            public Login? Login { get; set; }
 
             
             public Authenticator Convert()
             {
+                Authenticator ConvertFromInfo(AuthenticatorType type, string secret)
+                {
+                    return new()
+                    {
+                        Issuer = Name,
+                        Username = Login.Username,
+                        Type = type,
+                        Algorithm = Authenticator.DefaultAlgorithm,
+                        Digits = type.GetDefaultDigits(),
+                        Period = type.GetDefaultPeriod(),
+                        Icon = Icon.FindServiceKeyByName(Name),
+                        Secret = Authenticator.CleanSecret(secret, type)
+                    };
+                }
+                
                 if(Login.Totp.StartsWith("otpauth"))
                     return Authenticator.FromOtpAuthUri(Login.Totp);
-                
-                return new Authenticator()
+
+                if(Login.Totp.StartsWith("steam"))
                 {
-                    Issuer = Name,
-                    Username = Login.Username,
-                    Type = Type,
-                    Algorithm = Authenticator.DefaultAlgorithm,
-                    Digits = Type.GetDefaultDigits(),
-                    Period = Type.GetDefaultPeriod(),
-                    Icon = Icon.FindServiceKeyByName(Name),
-                    Secret = Authenticator.CleanSecret(Login.Totp, Type)
-                };
+                    var secret = Login.Totp.Substring("steam://".Length);
+                    return ConvertFromInfo(AuthenticatorType.SteamOtp, secret);
+                }
+                
+                return ConvertFromInfo(AuthenticatorType.Totp, Login.Totp);
             }
         }
 
