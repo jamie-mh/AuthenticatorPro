0b91a31c3252719fa15cc5c51576b0016514f3f6
Jamie Hodgson <jamie-hodgson@live.com>
2021-01-20 12:53:39 +0100

    make sure everything is cleaned up correctly when changing pass

--- AuthenticatorPro/Source/Data/Database.cs
+++ AuthenticatorPro/Source/Data/Database.cs
@@ -88,53 +88,62 @@ namespace AuthenticatorPro.Data
 
         public static async Task SetPassword(string currentPassword, string newPassword)
         {
-            var conn = await GetPrivateConnection(currentPassword);
             var dbPath = GetPath();
+            var backupPath = dbPath + ".backup";
+                
+            void DeleteDatabase()
+            {
+                File.Delete(dbPath);
+                File.Delete(dbPath.Replace("db3", "db3-shm"));
+                File.Delete(dbPath.Replace("db3", "db3-wal"));
+            }
+            
+            void RestoreBackup()
+            {
+                DeleteDatabase();
+                File.Move(backupPath, dbPath);
+            }
+            
+            File.Copy(dbPath, backupPath);
+            var conn = await GetPrivateConnection(currentPassword);
 
             // Change encryption mode
             if(currentPassword == null && newPassword != null || currentPassword != null && newPassword == null)
             {
-                var backupPath = dbPath + ".backup";
                 var tempPath = dbPath + ".temp";
-                
+
                 try
                 {
-                    await conn.BackupAsync(backupPath);
-
                     if(newPassword != null)
                         await conn.ExecuteAsync("ATTACH DATABASE ? AS temporary KEY ?", tempPath, newPassword);
                     else
                         await conn.ExecuteAsync("ATTACH DATABASE ? AS temporary KEY ''", tempPath);
 
                     await conn.ExecuteScalarAsync<string>("SELECT sqlcipher_export('temporary')");
-                    await conn.ExecuteAsync("DETACH DATABASE temporary");
+                }
+                catch
+                {
+                    File.Delete(tempPath);
+                    File.Delete(backupPath);
+                    throw;
                 }
                 finally
                 {
+                    await conn.ExecuteAsync("DETACH DATABASE temporary");
                     await conn.CloseAsync();
-                    File.Delete(backupPath);
-                }
-                
-                void DeleteDatabase()
-                {
-                    File.Delete(dbPath);
-                    File.Delete(dbPath.Replace("db3", "db3-shm"));
-                    File.Delete(dbPath.Replace("db3", "db3-wal"));
                 }
                 
                 try
                 {
                     DeleteDatabase();
                     File.Move(tempPath, dbPath);
-                    File.Delete(tempPath);
-                
                     conn = await GetPrivateConnection(newPassword);
                 }
                 catch
                 {
-                    // Restore backup
-                    DeleteDatabase();
-                    File.Move(backupPath, dbPath);
+                    // Perhaps it wasn't moved correctly
+                    File.Delete(tempPath);
+                    RestoreBackup();
                     throw;
                 }
                 finally
@@ -148,17 +157,35 @@ namespace AuthenticatorPro.Data
             {
                 // Cannot use parameters with pragma https://github.com/ericsink/SQLitePCL.raw/issues/153
                 var quoted = "'" + newPassword.Replace("'", "''") + "'";
-                
-                await conn.ExecuteAsync($"PRAGMA rekey = {quoted}");
-                await conn.CloseAsync();
-                
+
                 try
                 {
-                    conn = await GetPrivateConnection(newPassword);
+                    try
+                    {
+                        await conn.ExecuteAsync($"PRAGMA rekey = {quoted}");
+                    }
+                    finally
+                    {
+                        await conn.CloseAsync();
+                    }
+
+                    try
+                    {
+                        conn = await GetPrivateConnection(newPassword);
+                    }
+                    finally
+                    {
+                        await conn.CloseAsync();
+                    }
+                }
+                catch
+                {
+                    RestoreBackup();
+                    throw;
                 }
                 finally
                 {
-                    await conn.CloseAsync();
+                    File.Delete(backupPath);
                 }
             }
         }
