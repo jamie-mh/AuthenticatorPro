dda4aa1636b68efd731957ecff56a17ecd7e9658
Jamie Hodgson <jamie-hodgson@live.com>
2021-03-11 12:13:30 +0100

    init uwp assets

--- AuthenticatorPro.UWP/Properties/AssemblyInfo.cs
+++ AuthenticatorPro.UWP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AuthenticatorPro.Windows")]
+[assembly: AssemblyTitle("Authenticator Pro")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AuthenticatorPro.Windows")]
+[assembly: AssemblyProduct("Authenticator Pro")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
