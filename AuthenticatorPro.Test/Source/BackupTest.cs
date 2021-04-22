9b834c1d39b5724b43301388e6b1b40160d94502
Jamie Hodgson <jamie-hodgson@live.com>
2021-04-22 14:03:41 +0200

    add backup test

---
+++ AuthenticatorPro.Test/Source/BackupTest.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using AuthenticatorPro.Shared.Data.Backup;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace AuthenticatorPro.Test
+{
+    public class BackupTest
+    {
+        private readonly BackupComparer _backupComparer;
+        private readonly Backup _testBackup;
+        
+        public BackupTest()
+        {
+            _backupComparer = new BackupComparer();
+            
+            var contents = File.ReadAllText("test.authpro");
+            _testBackup = JsonConvert.DeserializeObject<Backup>(contents);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("t")]
+        [InlineData("test")]
+        [InlineData("test123")]
+        [InlineData("test123!?%")]
+        [InlineData("PZqE=_L]Ra;ZD8N&")]
+        [InlineData("tUT.3raAGQ[f]]Q@Ft=S}.r(Vk&CM9#`")]
+        [InlineData(@"MS^NqdNp&y]tLz_5:P;UU/2LDd_uF7a""x@*a't/Da]'y&b~.=&z3x'r^u{X.@?vv")]
+        [InlineData(@"p[{(]2QFSYWcaYdz=;eMtrnZ<bvh8QfW;8v""4HBTtW5H!xMGQKt^\\)f]7.fJ*9dcs@pq(9GKF?7FJ3Qtj$].V!U;:N^/eUj(zG;yC9/pPRGT'DHLR>S5h$}L\\AL~X,pA")]
+        [InlineData(@"#n*rF;y3zVPr,B3*qP""C,S[NFk""qswBC;k2*E_gQ&)4Rh.h[f;WG)5w3uY7MGvM/wngR]72BP9;{)_~+Vd""ukx'S]Zt[!cE='43f}J:J_TR<\:`w""{\+`dgdy%vD]Ts:xKAXzgRW!_7vWF]zg*u.)F#ZzzY&[LEHFXH(D@M7Y""'6.e7n~u""[4kyc'TGF28Q""xgNg.!5=;Hfx'e)fx,:#mLmkA(*ty]4]7#;^?*QF4xDK&Fx-)f}(ph=PL*N'#w9`")]
+        [InlineData("你好世界")]
+        [InlineData("😀 😃 😄 😁 😆 😅 😂 🤣")]
+        public void ToBytesFromBytesTest(string password)
+        {
+            var transformed = Backup.FromBytes(_testBackup.ToBytes(password), password);
+            Assert.Equal(transformed, _testBackup, _backupComparer); 
+        }
+    }
+}
---
+++ AuthenticatorPro.Test/Source/CustomIconComparer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AuthenticatorPro.Shared.Data;
+
+namespace AuthenticatorPro.Test
+{
+    public class CustomIconComparer : IEqualityComparer<CustomIcon>
+    {
+        public bool Equals(CustomIcon x, CustomIcon y)
+        {
+            if(ReferenceEquals(x, y)) return true;
+            if(ReferenceEquals(x, null)) return false;
+            if(ReferenceEquals(y, null)) return false;
+            if(x.GetType() != y.GetType()) return false;
+
+            return x.Id == y.Id && x.Data.SequenceEqual(y.Data);
+        }
+
+        public int GetHashCode(CustomIcon obj)
+        {
+            return HashCode.Combine(obj.Id, obj.Data);
+        }
+    }
+}
