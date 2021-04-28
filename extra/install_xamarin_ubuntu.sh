8367f02ed5c24927a71d141a7d5572c27cabee04
Jamie Hodgson <jamie-hodgson@live.com>
2021-04-28 16:07:21 +0200

    merge install

--- extra/install_xamarin_ubuntu.sh
+++ extra/install_xamarin_ubuntu.sh
@@ -13,13 +13,12 @@
 
 # Install prerequisites
 
-sudo apt -y install jq gdebi openjdk-8-jdk
+sudo apt -y install jq gdebi openjdk-8-jdk gnupg ca-certificates
 
 
 # Install Mono
 # https://www.mono-project.com/download/stable
 
-sudo apt install gnupg ca-certificates
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 sudo apt update
