a083d04fe20aff3fb2d5608ae3bf823afded7fa5
Jamie Hodgson <jamie-hodgson@live.com>
2021-04-28 14:45:21 +0200

    missing comment

--- extra/install_xamarin_ubuntu.sh
+++ extra/install_xamarin_ubuntu.sh
@@ -17,7 +17,7 @@ sudo apt -y install jq gdebi openjdk-8-jdk
 
 
 # Install Mono
-https://www.mono-project.com/download/stable
+# https://www.mono-project.com/download/stable
 
 sudo apt install gnupg ca-certificates
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
