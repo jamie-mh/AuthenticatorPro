bbb3b9b241c3ab5e333fc64ac3c8ee2be26fcb5a
Jamie Hodgson <jamie-hodgson@live.com>
2022-01-12 10:08:05 +0100

    Revert "use pngquant for optimisation"
    This reverts commit 02e125df2a633d5980defc1d54d8484cd5938bbb.

--- extra/optimise_icons.sh
+++ extra/optimise_icons.sh
@@ -5,4 +5,4 @@ set -e
 SCRIPT_PATH=`dirname "$0"`
 PROJECT_PATH=`realpath "$SCRIPT_PATH/../AuthenticatorPro.Droid.Shared"`
 
-pngquant --skip-if-larger --ext=.png --force --strip -- $PROJECT_PATH/Resources/**/*.png
+oxipng -o 4 -i 0 --strip safe $PROJECT_PATH/Resources/**/*.png
