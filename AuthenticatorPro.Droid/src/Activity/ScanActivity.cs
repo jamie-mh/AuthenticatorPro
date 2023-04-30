47ef69776f5264fa8eed4bb5d344035f39727cdc
Jamie Hodgson <jamie-hodgson@live.com>
2023-04-30 09:59:23 +0200

    lower target resolution

--- AuthenticatorPro.Droid/src/Activity/ScanActivity.cs
+++ AuthenticatorPro.Droid/src/Activity/ScanActivity.cs
@@ -41,7 +41,7 @@ namespace AuthenticatorPro.Droid.Activity
 
 #if FDROID
             var analysis = new ImageAnalysis.Builder()
-                .SetTargetResolution(new Size(1920, 1080))
+                .SetTargetResolution(new Size(1280, 720))
                 .SetBackpressureStrategy(ImageAnalysis.StrategyKeepOnlyLatest)
                 .SetOutputImageFormat(ImageAnalysis.OutputImageFormatRgba8888)
                 .Build();
